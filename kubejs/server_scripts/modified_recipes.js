8b6b2fdbdd2746ba2bf287e32fc4f05c0dea66b1
Vixeliz <vixeliz@protonmail.com>
2025-02-15 21:11:47 -0600

    fix for fabric

--- kubejs/server_scripts/modified_recipes.js
+++ kubejs/server_scripts/modified_recipes.js
@@ -15,7 +15,7 @@ ServerEvents.recipes(event => {
     event.recipes.create.sandpaper_polishing('minecraft:ender_pearl', 'minecraft:emerald')
 
     event.recipes.create.filling('minecraft:blaze_rod', [Fluid.lava(), 'minecraft:bone'])
-    event.recipes.create.filling('minecraft:netherrack', [Fluid.lava(50), '#forge:cobblestone'])
+    event.recipes.create.filling('minecraft:netherrack', [Fluid.lava(50), '#minecraft:stone_tool_materials'])
     event.recipes.create.mixing('minecraft:soul_sand', ['create:powdered_obsidian', '#minecraft:smelts_to_glass'])
     event.recipes.create.haunting('create:blaze_burner', 'create:empty_blaze_burner')
     event.recipes.create.haunting('minecraft:soul_campfire', 'minecraft:campfire')
