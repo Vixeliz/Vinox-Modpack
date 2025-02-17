b04701ac02db62a6c89c085b5986b9bb6ef94eb7
Vixeliz <vixeliz@protonmail.com>
2025-02-17 14:47:07 -0600

    A

--- kubejs/client_scripts/hidden_items.js
+++ kubejs/client_scripts/hidden_items.js
@@ -1,14 +1,14 @@
 JEIEvents.hideItems(event => {
   // Hide train stuff
-  event.hide('create:track')
-  event.hide('create:railway_casing')
-  event.hide('create:schedule')
-  event.hide('create:track_station')
-  event.hide('create:track_signal')
-  event.hide('create:track_observer')
-  event.hide('create:controls')
-  event.hide('create:train_door')
-  event.hide('create:train_trapdoor')
+  // event.hide('create:track')
+  // event.hide('create:railway_casing')
+  // event.hide('create:schedule')
+  // event.hide('create:track_station')
+  // event.hide('create:track_signal')
+  // event.hide('create:track_observer')
+  // event.hide('create:controls')
+  // event.hide('create:train_door')
+  // event.hide('create:train_trapdoor')
   
   // Hide unneeded mob drops
   event.hide('minecraft:rotten_flesh')
